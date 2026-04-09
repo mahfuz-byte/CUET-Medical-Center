85f06962d5204ef8eaec1cdd283b572e8899d294
mahfuz-byte <mahfuz.r5847@gmail.com>
2026-04-10 01:24:06 +0600

    Register models in Django admin (User, Doctor, Ambulance, Inventory, Medicine)

--- roster/admin.py
+++ roster/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Doctor, Ambulance
 
-# Register your models here.
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'title', 'dept', 'hours']
+    search_fields = ['name', 'dept', 'title']
+    list_filter = ['dept']
+
+
+@admin.register(Ambulance)
+class AmbulanceAdmin(admin.ModelAdmin):
+    list_display = ['ambulance_id', 'status', 'contact']
+    list_filter = ['status']
+    search_fields = ['ambulance_id', 'contact']
